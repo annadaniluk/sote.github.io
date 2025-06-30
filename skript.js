277feb7e6b5216f157925dcecf4897c2d18dfbd5
annadaniluk <114945996+annadaniluk@users.noreply.github.com>
2025-06-30 20:51:24 +0300

    Add files via upload

---
+++ skript.js
@@ -0,0 +1,34 @@
+const logbutton = document.getElementById("loginbutton");
+const logdiv = document.getElementById("logindiv");
+const xxbutton = document.getElementById("Xbutton");
+const imgdiv = document.getElementById("imgdivid");
+const imgsearch = document.querySelectorAll(".imgcl");
+const searchinput = document.getElementById("searchid");
+const schformid = document.getElementById("searchformid");
+
+logbutton.addEventListener("click", ()=>{ //робимо вікно видимим
+    logdiv.style.display = "block";
+});
+
+xxbutton.addEventListener("click", ()=>{ //робимо вікно невидимим
+    logdiv.style.display = "none";
+});
+
+searchinput.addEventListener("keyup", function(){
+    const keywords = searchinput.value.toLowerCase().trim();
+    imgsearch.forEach(item=>{
+    const display = item.dataset.tags.toLowerCase();
+    if(display.includes(keywords) || keywords===""){
+        item.classList.remove("hidden")
+    }
+    else{
+        item.classList.add("hidden");
+    }
+    });
+});
+
+if(schformid){
+    schformid.addEventListener("submit",function(event){
+        event.preventDefault();
+});
+}
